7a985c6f855296838f88171b60fd9f2ba1bea95d
JOSE <js959266@gmail.com>
2024-05-05 02:00:02 -0700

    jfn

--- networking/client.cc
+++ networking/client.cc
@@ -8,6 +8,60 @@
 using namespace std;
 using boost::asio::ip::tcp;
 
+int main(int argc, char* argv[]) {
+    try {
+        if (argc != 3) {
+            cerr << "Usage: client <host> <port>\n";
+            return EXIT_FAILURE;
+        }
+
+        boost::asio::io_context io_context;
+        tcp::iostream stream(argv[1], argv[2]);
+        
+        if (!stream) {
+            cerr << "Failed to connect to server: " << stream.error().message() << endl;
+            return EXIT_FAILURE;
+        }
+
+        cout << "Connected to server" << endl;
+        
+        thread([&stream] {
+            string message;
+            while (getline(stream, message)) {
+                cout << "Server: " << message << endl;
+            }
+        }).detach();
+
+        string input;
+        while (getline(cin, input)) {
+            stream << input << endl;
+            if (!stream) {
+                cerr << "Error sending message to server" << endl;
+                break;
+            }
+        }
+
+    } catch (exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
+
+
+
+
+/*#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+using boost::asio::ip::tcp;
+
 
 int main(int arg, char * argv[]){
 	try{
@@ -17,13 +71,16 @@ int main(int arg, char * argv[]){
 			return EXIT_FAILURE;
 		}
 		while(true){
-			string  s;
-			stream>>s;
+			string  input;
+//			cin<<input;
+			stream>>input;
+
 			if(!stream) break;
-			cout<<"client said: "<<s<<endl;
+			cout<<"server said: "<<input<<endl;
 			stream<<"hey its me, the client!"<<endl;
 		}
 	}
 	catch(exception& e){
 		cout<<"exception:"<<e.what()<<endl;}
 }
+*/
--- networking/server.cc
+++ networking/server.cc
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
-#include <unistd.h>
-#include <boost/bind/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include "../Map.h" // Include the Map class header file
+//#include <unistd.h>
+//#include <boost/bind/bind.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/enable_shared_from_this.hpp>
+#include <thread>
+#include <vector>
 
 using namespace std;
 using boost::asio::ip::tcp;
@@ -16,35 +17,50 @@ string daytime_string() {
 	return ctime(&now);
 }
 
-
+//make a funtion that will handle the clients
+void handle_client(tcp::iostream& stream){
+	try{
+		while(true){
+			//this pushed to the client screen
+			stream << "server" << endl;
+			string s;
+			stream >> s;
+			if (!stream) 
+		//break;
+				cout<<"error"<<endl;
+			cout << "server: " << s << endl;
+			usleep(333'333);
+		}
+	}
+	catch(exception& e){
+		cerr<<e.what()<<endl;}
+}
 
 
 int main(){
 	try {
-boost::asio::io_context io_context;
-	tcp::endpoint endpoint(tcp::v4(), 2101); //Change this port to whatever you want
-	tcp::acceptor acceptor(io_context, endpoint);
-	tcp::iostream stream;
-	boost::system::error_code error;
-	cout << "Waiting for the connection with the client"<<endl;
-	acceptor.accept(stream.socket(), error);
-	cout << "client connect!!!"<<endl;
+		boost::asio::io_context io_context;
+		tcp::endpoint endpoint(tcp::v4(), 2101); //Change this port to whatever you want
+		tcp::acceptor acceptor(io_context, endpoint);
+while(true){
+		tcp::iostream stream;
+		boost::system::error_code error;
+		
+cout << "Waiting for the connection with the client"<<endl;
+		acceptor.accept(stream.socket(), error);
 
-	if (!error) {
-		while (true) {
-			stream << "server" << endl;
-			string s;
-			stream >> s;
-			if (!stream) break;
-			cout << "server: " << s << endl;
-			usleep(333'333);
-		}
-	} else {
-		cout << "Sadge.\n";
+		if (!error) {
+		//	while (true) {
+				cout << "client connect!!!"<<endl;
+				thread(handle_client,std::ref(stream)).detach();
+		//	}
+		} else {
+			cout << "nope, this connect did not work: \n";
+		}}
+	//	io_context.run();
+	} catch (exception& e) {
+		cerr << e.what() << endl;
 	}
-} catch (exception& e) {
-	cerr << e.what() << endl;
-}
-
+return 0;
 }
 
