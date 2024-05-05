5b4ad1ef62633c5141b796e835812cf7c07af394
Kviale2004 <kviale2004@gmail.com>
2024-05-05 02:12:12 -0700

    Updating Riddle.cc

--- Riddle.cc
+++ Riddle.cc
@@ -4,15 +4,22 @@ using namespace std;
 
 int main() {
 
+	string y;
 	string x;
-	bool win = false
+	bool win = false;
+
+	cout << "Yo I'm Bencarelli, whats your name" << endl;
+	cin >> y;
+	cout << "Yeah I don't care" << endl;
+	cout << "now solve this riddle" << endl;
+	cout << endl;
 
 	cout << "Without a key, I hold people tight, A place to stay for those who fight. My walls are high, my gates are strong, I keep the guilty in for long. What am I." << endl;
 	cout << endl;
 	cin >> x;
-	if (x == "jail" || x == "prison") {
-		cout << "Correct heres a electricians manual" << endl;
-		win == true;
+	if (x == "jail" || x == "prison" || x == "Prison" || x == "Jail") {
+		cout << "Correct, here take this electricians manual" << endl;
+		win = true;
 		return 0;
 	}
 	else {
